11015247569434f7b8910ac75199d91d759ffb97
Cardona simon <simon.cdn@icloud.com>
2023-02-25 19:30:55 +0100

    💄  F01 : Add tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#81AFDD",
+        linearBackground: "#07164A",
+        background: "#040C18",
+        linearLeft: "#AE67FA",
+        linearRight: "#1BF1FF",
+      },
+      fontSize: {
+        mediumTxt: "20",
+      },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "825px",
+        md: "1110px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+      keyframes: {
+        levitation: {
+          "0%, 100%": { transform: "translateY(-20px)" },
+          "50%": { transform: "translateY(-50px)" },
+        },
+        shadowScale: {
+          "0%, 100%": { scale: "1" },
+          "50%": { scale: "0.7" },
+        },
+        sidebar: {
+          "0%": { transform: "translateY(50px)", opacity: "0" },
+          "100%%": { transform: "translateY(0px)", opacity: "1" },
+        },
+      },
+      animation: {
+        levitation: "levitation 6s linear infinite",
+        shadowScale: "shadowScale 6s linear infinite",
+        sidebar: "sidebar 1000ms ease-in-out ",
+      },
+    },
   },
   plugins: [],
-}
+};
