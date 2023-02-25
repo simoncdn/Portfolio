8704c83c2573feb522b55ded981b5707cf0efae0
Cardona simon <simon.cdn@icloud.com>
2023-02-25 19:32:37 +0100

    ✨ F01 : add Hero

---
+++ src/containers/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import styles from "../../style";
+import LeftSide from './LeftSide';
+import RightSide from './RightSide';
+
+export default function Hero() {
+  return (
+    <section className={`sm:grid-cols-12 sm:grid sm:my-0 my-6 relative ${styles.boxWidth} ${styles.padding} ${styles.flexCenter} flex flex-col-reverse `}>
+        <LeftSide />
+        <RightSide />
+    </section>
+  )
+}
