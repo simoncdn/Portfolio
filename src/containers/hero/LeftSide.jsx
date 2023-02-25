235b6cb43d5b9d58e41e183736a5b93e59c20558
Cardona simon <simon.cdn@icloud.com>
2023-02-26 00:07:19 +0100

    ♻️ F02 : HEADER / HERO > refacto

--- src/containers/hero/LeftSide.jsx
+++ src/containers/hero/LeftSide.jsx
@@ -6,12 +6,12 @@ export default function LeftSide() {
   return (
     <div className={`md:col-span-7 sm:col-span-8 flex justify-between flex-col sm:my-0 my-[30px]`}>
 
-        <h1 className="sm:text-[52px] md:text-[72px] text-[40px] gradient__text font-semibold ">
+        <h1 className= {`${styles.titleSize} gradient__text font-semibold`} >
             Turning problems<br />
             into solutions.<br />
         </h1>
 
-        <p className="text-primary mt-8 sm:w-[70%] w-[100%] font-mediumTxt ">Welcome ! My name is Simon, I'm front end developer located in France. I design and build website from scratch with love and passion.</p>
+        <p className={`${styles.textSize} text-primary mt-8 sm:w-[70%] w-[100%]`}  >Welcome ! My name is Simon, I'm front end developer located in France. I design and build website from scratch with love and passion.</p>
 
         <div className={`sm:w-[100%] sm:justify-start w-[100%] justify-center flex  ${styles.boxWidth} mt-[40px]`}>
             <div className="gradient__border rounded-3xl">
