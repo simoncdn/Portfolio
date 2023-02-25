167e2971e1ae121a2d2377f00cc5e37dee177813
Cardona simon <simon.cdn@icloud.com>
2023-02-25 19:31:44 +0100

    ✨ F01 : Add constant file

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+const navLinks = [
+  {
+    id: "technologies",
+    title: "Technologies",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export default navLinks;
